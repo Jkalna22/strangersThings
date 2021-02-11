d5d0b96f3ae2f495ba19a1aa9e291803fa1462d9
PallaviTalasila <73203401+PallaviTalasila@users.noreply.github.com>
2021-02-11 18:44:44 -0500

    Add files via upload
    starter file

---
+++ starter.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="app">
+        <div>"Hello World"</div>
+      </div>
+    );
+  }
+}
